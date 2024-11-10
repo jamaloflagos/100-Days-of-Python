f2469ecf0230c62818987ce3e318b2734300ac91
jamaloflagos <toyinjamal@gmail.com>
2024-11-10 22:38:27 +0100

    Day 8 of 100 Days of Python

--- Day_8/main.py
+++ Day_8/main.py
@@ -17,7 +17,7 @@ def encrypt(plain_text, shift_amount):
         new_letter = alphabets[new_position]
         cipher_text += new_letter
     
-    print(f"The encoded text is {cipher_text}")
+    print(f"The encoded text is: {cipher_text}")
 
 def decrypt(cypher_text, shift_amount):
     plain_text = ""
@@ -28,7 +28,7 @@ def decrypt(cypher_text, shift_amount):
         new_letter = alphabets[new_position]
         plain_text += new_letter
 
-    print(f"The decoded text is {plain_text}")
+    print(f"The decoded text is: {plain_text}")
 
 if direction == 'encode':
     encrypt(plain_text=message, shift_amount=shift)
