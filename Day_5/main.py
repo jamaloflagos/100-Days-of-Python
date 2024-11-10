c9de8bce40af03468d1f31cec8522655a4ae3e06
jamaloflagos <toyinjamal@gmail.com>
2024-11-10 01:04:19 +0100

    Day 5 of 100 days of Python

---
+++ Day_5/main.py
@@ -0,0 +1,33 @@
+import string
+import random
+uppercase_letters = list(string.ascii_uppercase)
+lowercase_letters = list(string.ascii_lowercase)
+uppercase_letters.extend(lowercase_letters)
+letters = uppercase_letters
+numbers = list(string.digits)
+symbols = list(string.punctuation)
+password_list = []
+password = ""
+
+print("Welcome to PyPassword Generator")
+amount_of_letters = int(input("How many letters do you want in your password?\n"))
+amount_of_numbers =int(input("How many numbers do you want?\n"))
+amount_of_symbols =int(input("How many symbols do you want?\n"))
+
+
+for char in range(0, amount_of_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, amount_of_numbers):
+    password_list.append(random.choice(numbers))
+
+for char in range(0, amount_of_symbols):
+    password_list.append(random.choice(symbols))
+
+random.shuffle(password_list)
+
+
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
